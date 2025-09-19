c77118869e56f84ba31ae674ce28b0d75eba8d5a
Arno Vaara <arnovaara@kirottu.com>
2025-09-19 18:05:10 +0300

    Moved substituters

--- modules/gaming/vr/default.nix
+++ modules/gaming/vr/default.nix
@@ -9,10 +9,6 @@
   options.gaming.vr.enable = lib.mkEnableOption "VR";
 
   config = lib.mkIf config.gaming.vr.enable {
-    nix.settings = {
-      substituters = [ "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-    };
 
     # Needed for WiVRn
     services.avahi = {

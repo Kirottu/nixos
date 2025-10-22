4f3a13f3ef97a7af176330929b34cdc1fc54961d
Arno Vaara <arnovaara@kirottu.com>
2025-10-22 09:05:27 +0300

    Temp Minecraft fix

--- modules/gaming/minecraft/default.nix
+++ modules/gaming/minecraft/default.nix
@@ -5,19 +5,19 @@
   ...
 }:
 let
-  glfw = pkgs.glfw3-minecraft.overrideAttrs {
-    patches = [
-      ./0001-Key-Modifiers-Fix.patch
-      ./0002-Fix-duplicate-pointer-scroll-events.patch
-      ./0003-Implement-glfwSetCursorPosWayland.patch
-      ./0004-Fix-Window-size-on-unset-fullscreen.patch
-      ./0005-Add-warning-about-being-an-unofficial-patch.patch
-      ./0006-Avoid-error-on-startup.patch
-      ./0007-Fix-fullscreen-location.patch
-      ./0008-Fix-forge-crash.patch
-    ];
+  # glfw = pkgs.glfw3-minecraft.overrideAttrs {
+  #   patches = [
+  #     ./0001-Key-Modifiers-Fix.patch
+  #     ./0002-Fix-duplicate-pointer-scroll-events.patch
+  #     ./0003-Implement-glfwSetCursorPosWayland.patch
+  #     ./0004-Fix-Window-size-on-unset-fullscreen.patch
+  #     ./0005-Add-warning-about-being-an-unofficial-patch.patch
+  #     ./0006-Avoid-error-on-startup.patch
+  #     ./0007-Fix-fullscreen-location.patch
+  #     ./0008-Fix-forge-crash.patch
+  #   ];
 
-  };
+  # };
 in
 {
   options.gaming.prismlauncher.enable = lib.mkEnableOption "Prismlauncher";
@@ -25,7 +25,7 @@ in
   config = lib.mkIf config.gaming.prismlauncher.enable (
     lib.utils.mkApp {
       package = pkgs.prismlauncher.override {
-        glfw3-minecraft = glfw;
+        # glfw3-minecraft = glfw;
       };
       userDirectories = [ ".local/share/PrismLauncher" ];
     }

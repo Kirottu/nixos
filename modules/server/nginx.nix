9428b6a3747b50d8cb61562abfe7701840439de1
Arno Vaara <arnovaara@kirottu.com>
2026-03-11 18:44:23 +0200

    Fix

--- modules/server/nginx.nix
+++ modules/server/nginx.nix
@@ -1,13 +1,12 @@
 { config, lib, ... }:
 {
-  config =
-    lib.mkIf config.devices.class == "server" {
-      impermanence.directories = [ "/var/lib/acme" ];
+  config = lib.mkIf (config.devices.class == "server") {
+    impermanence.directories = [ "/var/lib/acme" ];
 
-      services.nginx = {
-        enable = true;
-      };
-
-      users.users."nginx".extraGroups = [ "acme" ];
+    services.nginx = {
+      enable = true;
     };
+
+    users.users."nginx".extraGroups = [ "acme" ];
+  };
 }

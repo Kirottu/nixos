ad6bf3647224dc82ee6b35dcc896d11c379bca34
Arno Vaara <arnovaara@kirottu.com>
2026-07-25 22:55:41 +0300

    Stuff

--- modules/server/nextcloud.nix
+++ modules/server/nextcloud.nix
@@ -40,7 +40,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud33;
+      package = pkgs.nextcloud34;
       https = true;
       hostName = cfg.domain;
       maxUploadSize = "5G";
--- modules/server/syncserver.nix
+++ modules/server/syncserver.nix
@@ -26,6 +26,33 @@ in
 
     services.firefox-syncserver = {
       enable = true;
+      package = pkgs.syncstorage-rs.overrideAttrs (
+        old:
+        let
+          swaggerSrc = pkgs.fetchurl {
+            url = "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip";
+            hash = "sha256-SBJE0IEgl7Efuu73n3HZQrFxYX+cn5UU5jrL4T5xzNw=";
+          };
+
+          src = pkgs.fetchFromGitHub {
+            owner = "mozilla-services";
+            repo = "syncstorage-rs";
+            rev = "f084c3c78f91939a69ff10303f6579f7bf538beb";
+            hash = "sha256-d0rA/bWuo4gXvqI2inlvRI9NBP6ZRNSwLPkszNIkmhE=";
+          };
+        in
+        {
+          inherit src;
+          version = "0.23.3";
+
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            inherit src;
+            hash = "sha256-BJ5+6o57WlwsTerKCmOPXATPHQfjr5cRYMbqC8CIPg0=";
+          };
+
+          env.SWAGGER_UI_DOWNLOAD_URL = "file://${swaggerSrc}";
+        }
+      );
       singleNode = {
         inherit hostname;
         enable = true;

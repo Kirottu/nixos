b2a98a14dd4a4593626ed289bdc8b671e4397b00
Arno Vaara <arnovaara@kirottu.com>
2026-07-30 18:00:45 +0300

    Keycloak fix

--- modules/server/keycloak.nix
+++ modules/server/keycloak.nix
@@ -31,7 +31,39 @@ in
       plugins = [
         pkgs.keycloak.plugins.junixsocket-common
         pkgs.keycloak.plugins.junixsocket-native-common
-        pkgs.keycloak.plugins.keycloak-restrict-client-auth
+        # pkgs.keycloak.plugins.keycloak-restrict-client-auth
+        (pkgs.callPackage ({
+          maven,
+          lib,
+          fetchFromGitHub,
+        }:
+
+        maven.buildMavenPackage rec {
+          pname = "keycloak-restrict-client-auth";
+          version = "26.1.0";
+
+          src = fetchFromGitHub {
+            owner = "sventorben";
+            repo = "keycloak-restrict-client-auth";
+            tag = "v${version}";
+            hash = "sha256-nQ2AwXhSUu5RY/BbxXE2OXgEb7Zf6FfrGP5tfbgAXk8=";
+          };
+
+          mvnHash = "sha256-Q/UKZ4oe7T2pVRb8U0SoyvxUMgUn9grlDJPxlU9wLg4=";
+
+          installPhase = ''
+            runHook preInstall
+            install -Dm444 -t "$out" target/keycloak-restrict-client-auth.jar
+            runHook postInstall
+          '';
+
+          meta = {
+            homepage = "https://github.com/sventorben/keycloak-restrict-client-auth";
+            description = "Keycloak authenticator to restrict authorization on clients";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [ leona ];
+          };
+        }) {})
         # (pkgs.callPackage myPkgs.keycloak-unique-validator { })
       ];
       database = {
